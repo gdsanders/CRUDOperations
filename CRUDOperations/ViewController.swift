5f3f1c4fa7eb03491720e096d5cbbdd89aa3d49a
gdsanders <gd@digitalequity.com>
2015-10-15 16:11:14 -0500

    createPeople func added

--- CRUDOperations/ViewController.swift
+++ CRUDOperations/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -19,6 +20,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func createPeople(moc: NSManagedObjectContext) {
+        let names = ["Bob", "Anthony", "Evan", "Rickey"]
+        
+        for n in names {
+            guard let person = NSEntityDescription.insertNewObjectForEntityForName("Person", inManagedObjectContext: moc)
+                as? Person else
+            {continue}
+            person.name = n
+        }
+    }
+
 
 
 }
